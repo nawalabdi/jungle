eac88510e2f2bc3c0dc16ba4ecf6f2ade8c5c05b
nawalabdi <nawalabdi03@gmail.com>
2023-01-26 15:53:38 -0500

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+ 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create
+
+
   resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -18,6 +27,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -74,3 +85,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+# config/routes.rb
+

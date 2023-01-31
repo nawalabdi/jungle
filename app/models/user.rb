87490f94e4e0eff13f56252679e800ed82b86ae9
nawalabdi <nawalabdi03@gmail.com>
2023-01-31 14:05:17 -0500

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, confirmation: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.authenticate_with_credentials(email, password)
+    if user = self.where("lower(email) = ?", email.strip.downcase).first
+      user.authenticate(password)
+    end
+  end
 end
